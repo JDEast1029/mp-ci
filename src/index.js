14b6d906480cefa1efbdf2c39014f154d18ef040
蒋东 <852778359@qq.com>
2023-04-15 17:45:30 +0800

    feat: 推送到git仓库

--- src/index.js
+++ src/index.js
@@ -8,27 +8,50 @@ import { createChangeLog } from "./change-log.js";
 const ROOT_PATH = process.cwd()
 
 const options = minimist(process.argv.slice(2))
+const isDryRun = options.dry
 const step = msg => console.log(chalk.cyan(msg))
 const error = (msg) => console.log(chalk.red(msg))
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
+const dryRun = (bin, args, opts = {}) =>
+  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
+
+const runIfNotDry = isDryRun ? dryRun : run
 
 export const main = async () => {
 	try {
 		// dev
-		// const config = await getConfig(ROOT_PATH, options);
-		// const version = await createVersion(ROOT_PATH);
-		// config.upload.version = version;
+		const config = await getConfig(ROOT_PATH, options);
+		const targetVersion = await createVersion(ROOT_PATH);
+		config.upload.version = targetVersion;
 		
-		// step('\nUpdating package version...')
-		// updatePackageVersion(ROOT_PATH, version);
+		step('\nUpdating package version...')
+		updatePackageVersion(ROOT_PATH, targetVersion);
 		
-		// step('\nbuild package...')
+		step('\nbuild package...')
 		// await run('npm', ['run', config.script.build || 'build'])
 		
 
 		step('\nGenerating changelog...')
-		await createChangeLog(ROOT_PATH, options)
+		await createChangeLog(ROOT_PATH, config)
+
+		const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+		if (stdout) {
+			step('\nCommitting changes...')
+			await runIfNotDry('git', ['add', '-A'])
+			await runIfNotDry('git', ['commit', '-m', `release: v${targetVersion}`])
+		} else {
+			console.log('No changes to commit.')
+		}
+
+		step('\nPushing to GitHub...')
+		await runIfNotDry('git', ['tag', `v${targetVersion}`])
+		await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
+		await runIfNotDry('git', ['push'])
+		if (isDryRun) {
+			console.log(`\nDry run finished - run git diff to see package changes.`)
+		}
+		console.log()
 	} catch (e) {
 		error(e instanceof Error ? e.message : e);
 	}
