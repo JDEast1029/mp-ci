a35d95211be5d80c9f0e3d68fdcf81bf519295ec
蒋东 <852778359@qq.com>
2023-05-17 11:12:03 +0800

    fix: 不把'miniprogram-ci', 'conventional-changelog'打包到项目内

--- bin/mp-ci.js
+++ bin/mp-ci.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
-// const { main } = require('../lib/index.cjs')
 import { main } from '../lib/index.mjs'
-// import { main } from '../src/index.js'
 main();
--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ const config = {
 		file: 'lib/index.mjs',
 		format: 'esm',
 	}],
+	external: ['miniprogram-ci', 'conventional-changelog'],
 	plugins: [
 		nodeResolve({
 			exportConditions: ['node'],
